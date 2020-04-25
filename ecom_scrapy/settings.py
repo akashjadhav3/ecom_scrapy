ae7f7b583a56738f349c260adc9cc9aef6325689
root <root@localhost.localdomain>
2020-04-25 23:14:15 +0530

    adding user agent

--- ecom_scrapy/settings.py
+++ ecom_scrapy/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'ecom_scrapy.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ecom_scrapy (+http://www.yourdomain.com)'
+# USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -39,10 +39,9 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+# DEFAULT_REQUEST_HEADERS = {
+#   'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
--- ecom_scrapy/spiders/spacial_offer.py
+++ ecom_scrapy/spiders/spacial_offer.py
@@ -6,6 +6,10 @@ class SpacialOfferSpider(scrapy.Spider):
     name = 'spacial_offer'
     allowed_domains = ['www.tinydeal.com']
     start_urls = ['https://www.tinydeal.com/specials.html']
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.tinydeal.com/specials.html',callback=self.parse,headers={
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
@@ -13,10 +17,14 @@ class SpacialOfferSpider(scrapy.Spider):
                 'title':product.xpath(".//a[@class='p_box_title']/text()").get(),
                 'url':response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
                 'discounted_price':product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
-                'original_price':product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+                'original_price':product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(),
+                'User-Agent':response.request.headers['User-Agent']
+
             }
 
         next_page = response.xpath("//a[@class='nextPage']/@href").get()
 
         if next_page:
-            yield scrapy.Request(url=next_page,callback=self.parse) 
+            yield scrapy.Request(url=next_page,callback=self.parse,headers={
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+        }) 
